390c22f41099db19f9b4ae6ce9ae977a17bcc73b
TJ <cad.projekt.tj@gmail.com>
2025-01-08 11:34:50 +0100

    Add possibility of continuing the game

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -6,7 +6,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Game game = new Game(new Scanner(System.in));
-        game.chooseGame();
+        Scanner scanner = new Scanner(System.in);
+        GameSelector gameSelector = new GameSelector(new Scanner(System.in));
+
+        boolean keepPlaying;
+        do {
+            Game selectedGame = gameSelector.chooseGame();
+            if (selectedGame != null) {
+                System.out.println("Wybrano grę: " + selectedGame.getName());
+                selectedGame.start();
+            } else {
+                System.out.println("Nie wybrano poprawnie gry.");
+            }
+            System.out.println("Czy chcesz zagrać ponownie? (tak/nie)");
+            keepPlaying = scanner.next().equalsIgnoreCase("tak");
+
+        } while (keepPlaying);
     }
 }
